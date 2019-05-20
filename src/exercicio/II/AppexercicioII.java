a1358b4b6e6ebe399ec034b9cf2232db3ede0df0
Tiago Souza <tiago.souza@zup.com.br>
2019-05-20 02:05:51 -0300

    Program para ler idade em dias

---
+++ src/exercicio/II/AppexercicioII.java
@@ -0,0 +1,31 @@
+package exercicio.II;
+
+import java.util.Scanner;
+
+public class AppexercicioII {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		double ano = 365;
+		
+		double mes = 30;
+		
+		System.out.println("Informe idade ");
+		
+		double idade = entrada.nextDouble();
+		
+		double idadeDias =  (idade * ano ) ; 
+		
+		System.out.println("A idade em dias é " + idadeDias);
+		
+		
+		
+		
+      
+        
+        
+	}
+
+}
